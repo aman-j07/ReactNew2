1ed7fbe210e58fd1f747d26dda03b94981c299ec
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-23 15:54:12 +0530

    React Style 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import logo from './components/logoscience.png'
-import study from './components/study.png'
+import logo from './components/logo.png'
+import msg from './components/msg.png'
+import comp from './components/comp.png'
 function App() {
   return (
     <div className="App">
-      <div id="navBar"><img src={logo}/><div id="navLinks"><a>HOME</a><a>ABOUT US</a><a>SERVICE</a><a>CONTACT</a><a id="aLogIn">LOG IN</a></div></div>
-      <div id="body"><div id="left"><h2 id="online">ONLINE</h2><h2 id="education">EDUCATION</h2><p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temprr in- cididunt ut labore et dolore gna aliqua.</p><button>Learn More</button></div><div id="right"><img src={study}/></div></div>
+      <div id="navBar"><img src={logo}/><div id="navLinks"><a>Solutions</a><a>Products</a><a>Resources</a><a>Company</a><a id="aLogIn">Get Started</a><a><i class="fa-solid fa-magnifying-glass"/>Search</a></div></div>
+      <div id="body"><div id="left"><h2 id="online">Get Started with Apptio Today</h2><p>Talk to an Apptio expert about your specific needs and see a live product demonstration. Discuss current challenges and find the right application for your use case.</p></div><div id="right"><div><input placeholder='First Name'/><input placeholder='Last Name'/></div><div><input placeholder='Work Email'/><input placeholder='Phone'/></div><div><input placeholder='Company'/><select><option>Job Function</option></select></div><select id="inpSecLast"><option>Country</option></select><input id="inpLast" placeholder='How Can We Help?'/><button id="btnStarted">Get Started</button></div><div id="imgDivs"><img src={msg}/><img src={comp}/></div></div>
     </div>
   );
 }
