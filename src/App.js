0e47cef6d4f339bbf359c5891c32675c2f42a9ff
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-10-11 16:50:28 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import CloudIcon from '@mui/icons-material/Cloud';
 function App() {
   return (
     <div id="App">
-      <div id="navBar">
-        <CloudIcon/> <pre> WChecker</pre>
-      </div>
+      <div id="navBar"><CloudIcon/> <pre> WChecker</pre></div>
       <Weather/>
     </div>
   );
