c51d3881d427298b0939ab1d2dcfa1c9edd4a2a4
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-10-14 14:42:04 +0530

    Design Page

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -7,7 +7,7 @@ const Page1 = () => {
         <img src={img}/>
         <div id="signUpIn"><p id="signUp">Sign up</p><p id="signIn">Sign in</p></div>
         <form>
-            <input placeholder='Username'/><input placeholder='Full Name'/><input placeholder='Email'/><input placeholder='Password'/><div id="tnC"><input type="checkbox"/><label>Agree with</label><a>Terms & Conditions</a></div>
+            <input placeholder='Username'/><input placeholder='Full Name'/><input placeholder='Email'/><input placeholder='Password'/><div id="tnC"><div id="divCheck"><input type="checkbox"/></div><label>Agree with</label><a>Terms & Conditions</a></div>
         </form>
         <button id="btnSignUp">Sign Up</button>
         <a id="aMember">I'm already a amember</a>
