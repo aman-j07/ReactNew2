2183ccf569ef6e88e7f6ce050352c3371b67cec9
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-10-18 09:46:48 +0530

    Maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 function Map() {
+  const [cordinates, setCordinates] = useState({lat:26.8467,lng:80.9462});
+  let latRef=useRef();
+  let lngRef=useRef();
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDbvHwG-hVIkiJLlhoRWAmX3aeOlEzs0qo",
+  });
+
+  const changeMap=()=>{
+    let obj={lat:parseFloat(latRef.current.value),lng:parseFloat(lngRef.current.value)}
+    console.log(obj)
+    setCordinates(obj) 
+  } 
+
+  if (!isLoaded) return <div>Loading...</div>;
   return (
-    <div>Map</div>
-  )
+    <>
+      <div id="inputs"><input placeholder="Latitude" ref={latRef} /> <input placeholder="Longitude" ref={lngRef}/>
+      <button onClick={changeMap}>Submit</button>
+      </div>
+      <GoogleMap
+        zoom={10}
+        center={{ lat: cordinates.lat , lng: cordinates.lng }}
+        mapContainerClassName="map-container"
+      ></GoogleMap>
+    </>
+  );
 }
 
-export default Map
+export default Map;
