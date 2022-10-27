0bb483f12bdc1afc52b3f898212bc69e4ae1e68d
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-10-27 17:30:01 +0530

    Memory Game

--- src/components/MemoryGame.js
+++ src/components/MemoryGame.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import FlipCard from "./FlipCard";
 
 
@@ -7,9 +7,8 @@ let memory=[0, 0];
 let memoryClass=["",""];
 let values=[[1,"charizard.png"],[2,"beckham.jpg"],[3,"pikachu.png"],[4,"nike.jpeg"],[5,"greenlotus.webp"],[6,"CSS3_logo.png"],[7,"cr7logo.jpg"],[8,"HTML5_logo.png"],[9,"React-icon.svg.png"],[10,"waterthrower.webp"],[11,"javascript_logo.png"],[12,"Real_Madrid_CF.png"],[1,"charizard.png"],[2,"beckham.jpg"],[3,"pikachu.png"],[4,"nike.jpeg"],[5,"greenlotus.webp"],[6,"CSS3_logo.png"],[7,"cr7logo.jpg"],[8,"HTML5_logo.png"],[9,"React-icon.svg.png"],[10,"waterthrower.webp"],[11,"javascript_logo.png"],[12,"Real_Madrid_CF.png"]];
 const MemoryGame = () => {
-  const [congDiv,setCongDiv]=useState();
   const [steps,setSteps]=useState(0)
-
+  const refDiv=useRef();
   useState(()=>{
     for (let i = values.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -27,14 +26,13 @@ const MemoryGame = () => {
       memory[1] = ind;
       memoryClass[1]=e.target.closest(".flip-card-inner");
       e.target.closest(".flip-card-inner").classList.add("show");
+      setSteps(prev => prev+1);
       setTimeout( function() { checkSimilar(ind); }, 1000);
     } 
   };
 
   const checkSimilar=(ind)=>{
-    setSteps((prev) => prev + 1);
     if (parseInt(memory[0]) === parseInt(memory[1])) {
-      console.log("matched")
       for(let i=0;i<memoryClass.length;i++){
         memoryClass[i].parentNode.classList.add("hide")
       }
@@ -42,10 +40,9 @@ const MemoryGame = () => {
       memoryClass=["",""]
       cards[parseInt(ind-1)]=true;
       if(cards.every( (val) => val === true )){
-        setCongDiv(<div id="congDiv"><h2>Congratulations !!!</h2><p>You Won</p><p>You took {steps} steps</p></div>)
+          refDiv.current.classList.remove("hidden")
       } 
     } else if (parseInt(memory[0]) !== parseInt(memory[1])) {
-      console.log("Not matched")
       for(let i=0;i<memoryClass.length;i++){
         memoryClass[i].classList.remove("show")
       }
@@ -58,10 +55,10 @@ const MemoryGame = () => {
   return (
     <>
       <h2>Memory Game</h2>
-      {congDiv}
+      <div ref={refDiv} id="congDiv" className="hidden"><h2>Congratulations !!!</h2><p>You Won</p><p>You took {steps} steps.</p></div>
       <div className="container">
-        {values.map(item=>{
-          return(<FlipCard ind={item[0]}  onclick={show} img={item[1]} />)
+        {values.map((item,i)=>{
+          return(<FlipCard key={i} ind={item[0]}  onclick={show} img={item[1]} />)
         })}
       </div>
     </>
